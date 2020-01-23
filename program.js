2c64d3f9807a122b2640fe8674a21a6319e92951
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-23 23:33:53 +0530

    pug view engine setup

--- program.js
+++ program.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();  
 const process = require('process');
+const path = require('path')
 
 app.use(express.static('public'));
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+app.set('view engine', 'pug')
+app.set('views', process.argv[3])
 
-app.get('/home', function(req, res){
-  res.end('Hello World!');
+app.get('/home', (req, res) => {
+  res.render('index', {date : new Date().toDateString()});
 })
 app.listen(process.argv[2]);
