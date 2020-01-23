4c5253304cdf44646e1fd43d2a4ed3a0079929e3
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-23 23:19:01 +0530

    express static files via app.use

--- program.js
+++ program.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();  
 const process = require('process');
 
+app.use(express.static('public'));
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
 app.get('/home', function(req, res){
   res.end('Hello World!');
 })
