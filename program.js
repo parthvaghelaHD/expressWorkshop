2fa52e77d0782b6336a4be935cd689dc3c114cc6
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-24 00:03:26 +0530

    Param pam pam done...! in express

--- program.js
+++ program.js
@@ -4,13 +4,13 @@ const process = require('process');
 const path = require('path')
 const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({extended: false}))
-app.use(require('stylus').middleware(process.argv[3]))
+// app.use(require('stylus').middleware(process.argv[3]))
 
-app.use(express.static('public'));
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+// app.use(express.static('public'));
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
 // app.set('view engine', 'pug')
 // app.set('views', process.argv[3])
-app.use(express.static(process.argv[3]))
+// app.use(express.static(process.argv[3]))
 
 /*
   app.get('/home', (req, res) => {
@@ -22,4 +22,14 @@ app.use(express.static(process.argv[3]))
     res.send(req.body.str.split('').reverse().join(''))
   })
 */
+
+  app.put('/message/:id', (req, res, next)=>{
+    const id = req.params.id;
+    const str = require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex')
+    next();
+    res.send(str)
+  })
 app.listen(process.argv[2]);
